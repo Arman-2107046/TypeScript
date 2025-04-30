20e5c5219457f6b5fdab23fedaada297dd2e97c5
Arman-2107046 <armanr.rafi@gmail.com>
2025-04-30 16:58:14 +0600

    tuples done

---
+++ tuples.ts
@@ -0,0 +1,8 @@
+// let myTuple: [number, string];
+
+// myTuple=[10,'hello world'];
+// console.log(myTuple);
+
+// const games: [string, number, boolean]=['GTA V', 5, true];
+
+// console.log(games);
