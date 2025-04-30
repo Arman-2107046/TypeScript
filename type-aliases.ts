c61303a2dc65f1b9e1d79ff6c4f22c1fe3c4f58a
Arman-2107046 <armanr.rafi@gmail.com>
2025-04-30 15:06:34 +0600

    optional properties added with type alias

--- type-aliases.ts
+++ type-aliases.ts
@@ -1,22 +1,34 @@
-type User = {
-  name: string;
-  age: number;
-  location: string;
-};
+// type User = {
+//   name: string;
+//   age: number;
+//   location: string;
+// };
 
-const printUserInfo = (user: User) => {
-  return `Name: ${user.name}, Age: ${user.age}, Location: ${user.location}`;
-};
+// const printUserInfo = (user: User) => {
+//   return `Name: ${user.name}, Age: ${user.age}, Location: ${user.location}`;
+// };
 
-// const user: User = {
+// const res = printUserInfo({
 //   name: "arman",
 //   age: 21,
 //   location: "Dhaka",
-// };
-const res = printUserInfo({
-  name: "arman",
-  age: 21,
-  location: "Dhaka",
-});
+// });
 
-console.log(res);
+// console.log(res);
+
+
+
+
+type User={
+  name:string;
+  age?:number;
+  readonly location:string
+};
+
+const user:User={
+  name:"arman",
+  // age:21,
+  location:"dhaka"
+};
+
+console.log(user);
