6bc93d31c71b7784ee9c3d99f7e2a298323b4bcb
Arman-2107046 <armanr.rafi@gmail.com>
2025-04-30 13:36:32 +0600

    arrays done

---
+++ array.ts
@@ -0,0 +1,19 @@
+// const nums:number []=[1,2,3,4,5]
+// console.log(nums);
+
+// const str:string []=['apple','banana','potato'];
+// str.push('newElement');
+// console.log(str);
+
+// const items: Array<string>=[]
+// console.log(items);
+
+const singleDi : number[]=[1,2,3,4,5];
+const multiDi : number[][]=[
+[1,2,3],
+[4,5,6]
+];
+const triple : number[][][]=[[[1,2,3,4,5]]] 
+console.log(singleDi);
+console.log(multiDi);
+console.log(triple);
