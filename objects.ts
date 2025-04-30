7012553fd6cc035eb40aecb3fb07e183f9505300
Arman-2107046 <armanr.rafi@gmail.com>
2025-04-30 13:43:27 +0600

    objects done

---
+++ objects.ts
@@ -0,0 +1,18 @@
+// const person: { firstName: string; lastName: string; age: number } = {
+//   firstName: "Arman",
+//   lastName: "Rafi",
+//   age: 21,
+// };
+
+// console.log(person);
+
+function printUser():{name:string; age:number; location:string}
+{
+    return{
+        name:"Arman",
+        age:22,
+        location:"Dhaka"
+    }
+}
+
+console.log(printUser());
