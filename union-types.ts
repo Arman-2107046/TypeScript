f1ea3ffaf1af489ce70557e9888eec597ab04727
Arman-2107046 <armanr.rafi@gmail.com>
2025-04-30 16:47:31 +0600

    union types done

---
+++ union-types.ts
@@ -0,0 +1,25 @@
+// let password: string | number = "rafi";
+
+// type UserInfo = {
+//   first: string;
+//   last: string;
+//   age: number;
+// };
+
+// type AccountDetails = {
+//   email: string;
+//   password: string;
+// };
+
+// let user: UserInfo | AccountDetails = {
+//   first: "rafi",
+//   last: "arman",
+//   age: 21,
+// };
+
+// let user2: UserInfo | AccountDetails = {
+//   email: "rafi@gmail.com",
+//   password: "pass123",
+// };
+
+// const items: (number | string)[] = [1, 2, 3, "apple", 4, 5];
